917e2f5f6574e2b353b40e1e6e5dfc8f5163040c
nishaarya <nishaaryaahmed@gmail.com>
2020-05-08 18:51:40 +0100

    Ex1,2,4

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,36 +1,36 @@
 # Each ticket is represented as a class with the following form:
-class Ticket:
-    def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+class Ticket: 
+    def __init__(self, source, destination): 
+        self.source = source 
+        self.destination = destination 
 
 
-def reconstruct_trip(tickets, length):
-    ht = {}
-    # Empty spaces for the hash table
-    route = [None] * length
+def reconstruct_trip(tickets, length): 
+    ht = {} 
+    # Empty spaces for the hash table 
+    route = [None] * length 
 
-    # iterate over each ticket
-    for ticket in tickets:
-        # put the ticket.source as key - starting airport
-        # put ticket.destination as the value - next airport
-        ht[ticket.source] = ticket.destination
+    # iterate over each ticket 
+    for ticket in tickets: 
+        # put the ticket.source as key - starting airport 
+        # put ticket.destination as the value - next airport 
+        ht[ticket.source] = ticket.destination 
 
-    # set the current_destination, to start off the list
-    current_destination = ht["NONE"]
+    # set the current_destination, to start off the list 
+    current_destination = ht["NONE"] 
 
-    # set an index
-    i = 0
-    while current_destination != "NONE":
-        route[i] = current_destination
-        current_destination = ht[current_destination]
-        # move on to the next iteration
-        i += 1
-    route[i] = "NONE"
+    # set an index 
+    i = 0 
+    while current_destination != "NONE": 
+        route[i] = current_destination 
+        current_destination = ht[current_destination] 
+        # move on to the next iteration 
+        i += 1 
+    route[i] = "NONE" 
     
-    print(ht)
-    print(route)
+    print(ht) 
+    print(route) 
 
-    return route
+    return route 
 
     
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,20 +1,20 @@
-def has_negatives(a):
-    ht = {}
-    result = []
+def has_negatives(a): 
+    ht = {} 
+    result = [] 
 
-    # iterate over each number
-    for i in a:
-        ht[i] = None
-    for i in a:
-        if i > 0:
-            # positive * negative = negative
-            if i * -1 in ht:
-                # pass the value onto the results list
-                result.append(i)
+    # iterate over each number 
+    for i in a: 
+        ht[i] = None 
+    for i in a: 
+        if i > 0: 
+            # positive * negative = negative 
+            if i * -1 in ht: 
+                # pass the value onto the results list 
+                result.append(i) 
 
 
    
-    return result
+    return result 
 
 
 if __name__ == "__main__":
